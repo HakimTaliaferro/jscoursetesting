68f0954d24e1213c85449f1fece16d07f953d00e
HakimTaliaferro <taliaferro.hakim@gmail.com>
2025-02-28 15:22:22 -0500

    Reset counter added

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -1,15 +1,15 @@
 let count = 0; // Initialize count to 0
 
 function increaseCount() {
-  count++; // Increment the count by 1
-  displayCount(); // Display the count
-  checkCountValue(); // Check count value and display messages
+    count++; // Increment the count by 1
+    displayCount(); // Display the count
+    checkCountValue(); // Check count value and display messages
 }
+
 
 function displayCount() {
 document.getElementById('countDisplay').innerHTML=count; // Display the count in the HTML
 }
-
 function checkCountValue() {
     if (count === 10) {
       alert("Your Instagram post gained 10 followers! Congratulations!");
@@ -18,3 +18,8 @@ function checkCountValue() {
     }
   }
 
+function resetCount(){
+    count=0
+    displayCount(); // Display the count
+    alert("Followers count has been reset."); // Added alert message for reset
+}
